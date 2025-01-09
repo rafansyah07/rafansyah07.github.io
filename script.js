274ade809f9cda89989a84516af4e653dfd6e6e2
AlephSc <alephsc25@gmail.com>
2025-01-09 09:13:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-document.getElementById('obfuscateBtn').addEventListener('click', () => {
-  const suki = document.getElementById('luaInput').value;
-  if (!suki.trim()) {
-    alert('Please enter a Lua script to obfuscate!');
-    return;
-  }
-  
-  const obfuscated = suki
-    .split('')
-    .map(char => char.charCodeAt(0).toString(10))
-    .join('\\');
-
-  document.getElementById('luaOutput').value = `-- Obfuscated Lua Script --\nload([[\\${obfuscated}]])()`;
-});
+function p(str_init) {
+    alert(str_init);
+}
